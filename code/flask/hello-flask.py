78e434e9fe516ac50239b82a8184323887de7992
mForcella <michael.forcella@gmail.com>
2015-02-16 13:52:17 -0500

    Updated hello-flask.py to display temp in main.html.

--- code/flask/hello-flask.py
+++ code/flask/hello-flask.py
@@ -1,9 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import glob
+import time
+
 app = Flask(__name__)
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28-*')[0]
+device_file = device_folder + '/w1_slave'
+
+con = None
+
+def get_time():
+   return time.strftime("%Y-%m-%d %H:%M")
+
+def read_temp_raw():
+   f = open(device_file, 'r')
+   lines = f.readlines()
+   f.close()
+   return lines
 
 @app.route("/")
-def hello():
-   return "Hello face!"
+def read_temp():
+   now = get_time()
+   lines = read_temp_raw()
+   while lines[0].strip()[-3:] != 'YES':
+      time.sleep(0.2)
+      lines = read_temp_raw()
+   equals_pos = lines[1].find('t=')
+   if equals_pos != -1:
+      temp_string = lines[1][equals_pos+2:]
+      temp_c = float(temp_string) / 1000.0
+      temp_f = temp_c * 9.0 / 5.0 + 32.0
+   templateData = {
+      'title':'temp!',
+      'time':now,
+      'tempc':temp_c,
+      'tempf':temp_f
+   }
+   return render_template('main.html',**templateData)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0',port=80,debug=True)
