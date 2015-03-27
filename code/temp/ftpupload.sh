b5aebd102e6447bd82f0d1aca460363f342cf174
mForcella <michael.forcella@gmail.com>
2015-03-27 15:04:37 -0400

    Modified temp recording files.

--- code/temp/ftpupload.sh
+++ code/temp/ftpupload.sh
@@ -2,4 +2,4 @@
 
 sshpass -p 'Jezebel32' scp /home/pi/dev/git/ELSpring2015/code/temp/data/temp.csv forcel96@shell01.acs.newpaltz.edu:/home/forcel96/WWW/temp
 
-sshpass -p 'Jezebel32' scp /home/pi/dev/git/ELSpring2015/code/temp/data/latestTemp.csv forcel96@shell01.acs.newpaltz.edu:/home/forcel96/WWW/temp
+sshpass -p 'Jezebel32' scp /home/pi/dev/git/ELSpring2015/code/temp/data/latest_temp.csv forcel96@shell01.acs.newpaltz.edu:/home/forcel96/WWW/temp
